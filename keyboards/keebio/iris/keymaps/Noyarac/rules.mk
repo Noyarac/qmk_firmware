bc56859cb669f3ca9d202fa51ab4613f9da0cf1c
Benjamin Carayon <benjamin.carayon2025@campus-eni.fr>
2025-08-24 12:07:36 +0200

    rules modified

--- keyboards/keebio/iris/keymaps/Noyarac/rules.mk
+++ keyboards/keebio/iris/keymaps/Noyarac/rules.mk
@@ -1,5 +1,5 @@
+# Non-utilisés
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -7,11 +7,14 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-SPLIT_KEYBOARD = yes
 ENCODER_ENABLE = no
 
-KEY_OVERRIDE_ENABLE = yes
+# Utilisés
+MOUSEKEY_ENABLE = yes
+SPLIT_KEYBOARD = yes
 CAPS_WORD_ENABLE = yes
 
+# À vérifier
+KEY_OVERRIDE_ENABLE = yes
 LTO_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
